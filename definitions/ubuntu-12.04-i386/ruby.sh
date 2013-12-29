3b3f0f84e45961e9195301d6dd602efdd0e97a4d
Thomas Boerger <tboerger@tbpro.de>
2013-12-29 21:39:42 +0100

    Replaced ubuntu rbenv eith system packages

--- definitions/ubuntu-12.04-i386/ruby.sh
+++ definitions/ubuntu-12.04-i386/ruby.sh
@@ -1,7 +1,1 @@
-git clone https://github.com/sstephenson/ruby-build.git /var/ruby-build
-
-pushd /var/ruby-build > /dev/null
-bash install.sh
-popd > /dev/null
-
-/usr/local/bin/ruby-build 2.0.0-p247 /usr/local
+apt-get -y install ruby ruby-dev rubygems --fix-missing
