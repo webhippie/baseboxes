cb956ebc83b1b09a22f7910d4a8dc76aac5f4dde
Thomas Boerger <tboerger@tbpro.de>
2014-04-28 14:58:36 +0200

    Added debian 7.4 and updated ruby installation to debian packages instead of ruby-install

--- definitions/debian-7.3-i386/ruby.sh
+++ definitions/debian-7.3-i386/ruby.sh
@@ -1,13 +1,16 @@
-apt-get -y install libxml2-dev libxslt-dev
-wget -O /tmp/ruby-install-0.3.4.tar.gz https://github.com/postmodern/ruby-install/archive/v0.3.4.tar.gz
+apt-get -y install libxml2-dev libxslt-dev ruby1.9.1 ruby1.9.1-dev
+REALLY_GEM_UPDATE_SYSTEM=1 gem1.9.1 update --system
 
-pushd /tmp
-tar -xzvf ruby-install-0.3.4.tar.gz
+# apt-get -y install libxml2-dev libxslt-dev
+# wget -O /tmp/ruby-install-0.3.4.tar.gz https://github.com/postmodern/ruby-install/archive/v0.3.4.tar.gz
 
-pushd /tmp/ruby-install-0.3.4
-make install
-popd
-popd
+# pushd /tmp
+# tar -xzvf ruby-install-0.3.4.tar.gz
 
-rm -rf /tmp/ruby-install-0.3.4
-/usr/local/bin/ruby-install -i /usr/local ruby 2.0.0-p353
+# pushd /tmp/ruby-install-0.3.4
+# make install
+# popd
+# popd
+
+# rm -rf /tmp/ruby-install-0.3.4
+# /usr/local/bin/ruby-install -i /usr/local ruby 2.0.0-p353
