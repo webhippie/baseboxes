bc811a54c0cdaf6723ddb64c2c6a93f874b666a6
Thomas Boerger <thomas@webhippie.de>
2014-10-05 15:19:09 +0200

    Removed sleep from base script

--- scripts/opensuse/13.1/base.sh
+++ scripts/opensuse/13.1/base.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
-sleep 10
-
-zypper --non-interactive rm virtualbox-guest-kmp-default \
-  virtualbox-guest-kmp-desktop virtualbox-guest-kmp-pae \
-  virtualbox-guest-tools virtualbox-guest-x11
+zypper --non-interactive rm \
+  virtualbox-guest-tools \
+  virtualbox-guest-x11 \
+  virtualbox-guest-kmp-default \
+  virtualbox-guest-kmp-desktop \
+  virtualbox-guest-kmp-pae
 
 exit 0
