0862b6ae7887d97a5ff2ff28f4965f6448101e23
Thomas Boerger <thomas@webhippie.de>
2017-05-10 13:20:15 +0200

    Install centos packages on multiline

--- scripts/centos/6/base.sh
+++ scripts/centos/6/base.sh
@@ -17,8 +17,27 @@ enabled=1
 gpgcheck=0
 EOM
 
-yum -y install gcc make gcc-c++ kernel-devel zlib-devel openssl-devel fuse-utils
-yum -y install readline-devel sqlite-devel perl wget dkms nfs-utils git bzip2
+yum -y install \
+  gcc \
+  make \
+  gcc-c++ \
+  kernel-devel \
+  zlib-devel \
+  openssl-devel \
+  fuse-utils \
+  readline-devel \
+  sqlite-devel \
+  perl \
+  wget \
+  dkms \
+  nfs-utils \
+  git \
+  bzip2 \
+  epel-release \
+  cloud-init \
+  dracut-modules-growroot
+
+rpm -qa kernel | sed 's/^kernel-//'  | xargs -I {} dracut -f /boot/initramfs-{}.img {}
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
